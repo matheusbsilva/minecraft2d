873e3e9ac28fe771545a40a9caf19f9e27e0c610
matheusbsilva <matheusbattista@hotmail.com>
2019-04-09 11:26:16 -0300

    implement player moviment to all directions

--- game.py
+++ game.py
@@ -17,6 +17,12 @@ MAPHEIGHT = 20
 ASSETS_PATH = './assets/'
 
 tilemap = [[WATER for w in range(MAPWIDTH)] for h in range(MAPHEIGHT)]
+inventory = {
+        DIRT: 0,
+        GRASS: 0,
+        WATER: 0,
+        COAL: 0
+    }
 
 for row in range(MAPHEIGHT):
     for col in range(MAPWIDTH):
@@ -41,7 +47,7 @@ textures = {
 
 pygame.init()
 
-DISPLAYSURF = pygame.display.set_mode((MAPWIDTH*TILESIZE, MAPHEIGHT*TILESIZE))
+DISPLAYSURF = pygame.display.set_mode((MAPWIDTH*TILESIZE, MAPHEIGHT*TILESIZE + 50))
 PLAYER = pygame.image.load(ASSETS_PATH + 'player.png').convert_alpha()
 player_pos = [0, 0]
 
@@ -54,9 +60,20 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-        elif event.type == KEYDOWN and player_pos[0] < MAPWIDTH - 1:
-            if event.key == pygame.K_RIGHT:
+        elif event.type == KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                current_tile = tilemap[player_pos[1]][player_pos[0]]
+                inventory[current_tile] += 1
+                tilemap[player_pos[1]][player_pos[0]] = DIRT
+                print('[INVENTORY]', inventory)
+            elif event.key == pygame.K_RIGHT and player_pos[0] < MAPWIDTH - 1:
                 player_pos[0] += 1
+            elif event.key == pygame.K_LEFT and player_pos[0] > 0:
+                player_pos[0] -= 1
+            elif event.key == pygame.K_UP and player_pos[1] > 0:
+                player_pos[1] -= 1
+            elif event.key == pygame.K_DOWN and player_pos[1] < MAPHEIGHT - 1:
+                player_pos[1] += 1
 
     for row in range(MAPHEIGHT):
         for column in range(MAPWIDTH):
